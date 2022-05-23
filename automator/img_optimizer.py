201dd36b9fa259774b9e73ec94601750be9e37f0
LukeRouleau <jjp.adventures.22@gmail.com>
2022-05-23 19:16:31 -0400

    Yosemite Optimization

--- automator/img_optimizer.py
+++ automator/img_optimizer.py
@@ -22,6 +22,7 @@ downloads_path = 'C:/Users/lucas/Downloads'
 compressed_zip_name = 'C:/Users/lucas/Downloads/imagecompressor.zip'
 compression_dir = './post_compression'
 conversion_dir = './post_conversion'
+pre_process_dir = './pics_to_compress'
 smallest_dir = './maximally_minimized'
 
 '''
@@ -75,11 +76,15 @@ def convert_to_webp(source, dest):
     image = Image.open(compression_dir + '/' + source)  # Open image
     image.save(destination, format="webp")  # Convert image to webp
     return destination
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
 '''
 =================================== IMG COMPRESSION ===================================
 '''
 web = webdriver.Chrome()        # The Chrome Driver
-web.get('https://imagecompressor.com/')
+#web.get('https://imagecompressor.com/')
 
 # Empty the working directories
 for f in listdir(compression_dir):
@@ -95,67 +100,90 @@ download_all = '//*[@id="app"]/section[1]/div[3]/button'
 # Try with JS injection
 
 
-dir_path = path.abspath('./pics_to_compress') 
-files = next(walk('./pics_to_compress'), (None, None, []))[2]  # [] if no file
+dir_path = path.abspath(pre_process_dir) 
+files = next(walk(pre_process_dir), (None, None, []))[2]  # [] if no file
 
 print('Uploading the pictures to https://imagecompressor.com/, please wait...')
-drag_drop = web.find_element(By.XPATH, drag_and_drop)
-for file in files:
-    drag_and_drop_file(drag_drop, path.join(dir_path, file))
+#drag_drop = web.find_element(By.XPATH, drag_and_drop)
+for group in chunker(files, 20):
+    web.get('https://imagecompressor.com/')
+    drag_drop = web.find_element(By.XPATH, drag_and_drop)
+    for file in group:
+        drag_and_drop_file(drag_drop, path.join(dir_path, file))
 
-print('Waiting for the compression to occur, please wait...')
-# Wait until all have finished processing so that you can download them automatically:
-download_btn = WebDriverWait(web, 500).until(
-EC.element_to_be_clickable((By.XPATH, download_all)))
-download_btn.click();
+    print('Waiting for the compression to occur, please wait...')
+    # Wait until all have finished processing so that you can download them automatically:
+    download_btn = WebDriverWait(web, 500).until(
+    EC.element_to_be_clickable((By.XPATH, download_all)))
+    download_btn.click();
 
-# Wait until the zip is done downloading
-print('Waiting for the zip to download, please wait...')
-while not path.exists(compressed_zip_name):
-    time.sleep(1)
+    # Wait until the zip is done downloading
+    print('Waiting for the zip to download, please wait...')
+    while not path.exists(compressed_zip_name):
+        time.sleep(1)
 
-if path.isfile(compressed_zip_name):
-    print('Compressed images downloaded as ' + compressed_zip_name)
+    if path.isfile(compressed_zip_name):
+        print('Compressed images downloaded as ' + compressed_zip_name)
+        
+        # extract file
+        print('Extracting compressed images to ./post_compression...')
     
-    # extract file
-    print('Extracting compressed images to ./post_compression...')
- 
-    # Extract all the contents of zip file in different directory
-    with ZipFile(compressed_zip_name, 'r') as zipObj:
-        zipObj.extractall(compression_dir)
+        # Extract all the contents of zip file in different directory
+        with ZipFile(compressed_zip_name, 'r') as zipObj:
+            zipObj.extractall(compression_dir)
 
-    # delete zip
-    print('Deleting the zip...')
-    remove(compressed_zip_name)
+        # delete zip
+        print('Deleting the zip...')
+        remove(compressed_zip_name)
 
-else:
-    raise ValueError("%s isn't a file!" % compressed_zip_name)
-
-# Now, convert the images to the webp type
-print('Converting the compressed images to webp type, please wait...')
-files = next(walk(compression_dir), (None, None, []))[2]  # [] if no file
-for file in files:
-    convert_to_webp(file, conversion_dir)
-
-# Determine the Maximally Minimized Versions & Write out:
-files = next(walk(compression_dir), (None, None, []))[2]  # [] if no file
-for file in files:
-    no_ext = path.splitext(file)[0]
-    compressed_size = path.getsize(path.join(compression_dir,file))
-    webp_compressed_size = path.getsize(path.join(conversion_dir,no_ext + '.webp'))
-    if compressed_size < webp_compressed_size:
-        # Copy file to the maximally_minimized
-        shutil.move(compression_dir + '/' + file, smallest_dir + '/' + file)
     else:
-        # Copy the webp file to the maximally_minimized
-        curr_file = no_ext + '.webp'
-        shutil.move(conversion_dir + '/' + curr_file, smallest_dir + '/' + curr_file)
+        raise ValueError("%s isn't a file!" % compressed_zip_name)
 
-# Empty the working directories
-for f in listdir(compression_dir):
-    remove(path.join(compression_dir, f))
+    # Now, convert the images to the webp type
+    print('Converting the compressed images to webp type, please wait...')
+    files = next(walk(compression_dir), (None, None, []))[2]  # [] if no file
+    for file in files:
+        convert_to_webp(file, conversion_dir)
 
-for f in listdir(conversion_dir):
-    remove(path.join(conversion_dir, f))
+    # Determine the Maximally Minimized Versions & Write out:
+    files = next(walk(compression_dir), (None, None, []))[2]  # [] if no file
+    for file in files:
+        no_ext = path.splitext(file)[0]
+        compressed_size = path.getsize(path.join(compression_dir,file))
+        webp_compressed_size = path.getsize(path.join(conversion_dir,no_ext + '.webp'))
+        if compressed_size < webp_compressed_size:
+            # Copy file to the maximally_minimized
+            shutil.move(compression_dir + '/' + file, smallest_dir + '/' + file)
+        else:
+            # Copy the webp file to the maximally_minimized
+            curr_file = no_ext + '.webp'
+            shutil.move(conversion_dir + '/' + curr_file, smallest_dir + '/' + curr_file)
+
+    # Empty the working directories
+    for f in listdir(compression_dir):
+        remove(path.join(compression_dir, f))
+
+    for f in listdir(conversion_dir):
+        remove(path.join(conversion_dir, f))
 
 print('Success...')
+
+
+'''
+Total Reduction Calculation 
+'''
+pre_compressed_sum = 0
+post_compressed_sum = 0
+
+files = next(walk(pre_process_dir), (None, None, []))[2]  # [] if no file
+for file in files:
+    pre_compressed_sum += path.getsize(path.join(pre_process_dir,file))
+
+files = next(walk(smallest_dir), (None, None, []))[2]  # [] if no file
+for file in files:
+    post_compressed_sum += path.getsize(path.join(smallest_dir,file))
+
+reduction_val = ((pre_compressed_sum - post_compressed_sum) / pre_compressed_sum) * 100
+print('The images have been reduced by ', end='')
+print(reduction_val, end='')
+print('%')
